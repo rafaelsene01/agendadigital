3beb14cada67a6f8b8aad93f87591b3a2d036a02
Rafael Sene <rafaelssene@gmail.com>
2019-01-21 14:46:36 -0200

    Carga do Projeto

---
+++ src/main/java/br/com/iftm/AgendadigitalApplication.java
@@ -0,0 +1,14 @@
+package br.com.iftm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadigitalApplication.class, args);
+	}
+
+}
+
