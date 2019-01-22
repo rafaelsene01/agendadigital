de2bde23b79fad5d453a504fc8f1b42d6832fb76
Rafael Sene <rafaelssene@gmail.com>
2019-01-22 16:09:52 -0200

    Refatoracao das classes business e dao

--- src/main/java/br/com/iftm/controller/TipoServicoRest.java
+++ src/main/java/br/com/iftm/controller/TipoServicoRest.java
@@ -2,75 +2,126 @@ package br.com.iftm.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+
+import javax.websocket.server.PathParam;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.iftm.business.BusinessException;
+import br.com.iftm.business.TipoServicoBusiness;
+import br.com.iftm.business.Impl.TipoServicoBusinessImpl;
 import br.com.iftm.entity.TipoServico;
 
-@RestController  //Habilita classe como um servico rest.
-@RequestMapping(value="tiposervico") //Nome do servico.
+@RestController // Habilita classe como um servico rest.
+@RequestMapping(value = "tiposervico") // Nome do servico.
 public class TipoServicoRest {
-	
-	private List<TipoServico> lista = new ArrayList<>();
-	private int indice = 0;
 
-	//create
+	private List<TipoServico> lista = new ArrayList<>();
+	private TipoServicoBusiness business = new TipoServicoBusinessImpl();
+
+	// create
 	@PostMapping()
-	public ResponseEntity<?> create(@RequestBody TipoServico tipoServico){
-		if(StringUtils.isEmpty(tipoServico.getNome()))
-			return ResponseEntity.badRequest().body("Nome Requerido!");
-		
-		tipoServico.setCodigo(indice++);
-		lista.add(tipoServico);
-		return ResponseEntity.ok(tipoServico);
+	public ResponseEntity<?> create(@RequestBody TipoServico tipoServico) {
+
+		try {
+			tipoServico = business.create(tipoServico);
+
+			return ResponseEntity.ok(tipoServico);
+		} catch (BusinessException e) {
+			e.printStackTrace();
+
+			return ResponseEntity.badRequest().body(e);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return ResponseEntity.badRequest().body(e);
+		}
+
 	}
-	
-	//read
+
+	// read
 	@GetMapping
-	public ResponseEntity<?> read(){
-		return ResponseEntity.ok(lista);
+	public ResponseEntity<?> read() {
+		try {
+			return ResponseEntity.ok(business.read());
+
+		} catch (BusinessException e) {
+			e.printStackTrace();
+
+			return ResponseEntity.badRequest().body(e);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return ResponseEntity.badRequest().body(e);
+		}
+
 	}
-	
-	//update
-    @PutMapping
-    public ResponseEntity<?> update(@RequestBody TipoServico tipoServico){
-    	if(StringUtils.isEmpty(tipoServico.getNome()))
-			return ResponseEntity.badRequest().body("Nome Requerido!");
-		if(tipoServico.getCodigo() == null)
-			return ResponseEntity.badRequest().body("Codigo Requerido!");
-		
-		for (TipoServico tipoServico2 : lista) {
-			
-			if(tipoServico2.getCodigo().equals(tipoServico.getCodigo()))
-				tipoServico2.setNome(tipoServico.getNome());
+
+	// update
+	@PutMapping
+	public ResponseEntity<?> update(@RequestBody TipoServico tipoServico) {
+
+		try {
+			tipoServico = business.update(tipoServico);
+
+			return ResponseEntity.ok(tipoServico);
+		} catch (BusinessException e) {
+			e.printStackTrace();
+
+			return ResponseEntity.badRequest().body(e);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return ResponseEntity.badRequest().body(e);
 		}
-		return ResponseEntity.ok(tipoServico);
-    }
-	
-	//delete
-    @DeleteMapping
-    public ResponseEntity<?> delete(@RequestBody TipoServico tipoServico){
-    	if(tipoServico.getCodigo() == null)
-			return ResponseEntity.badRequest().body("Codigo Requerido!");
-    	
-    	for (TipoServico tipoServico2 : lista) {
-			
-			if(tipoServico2.getCodigo().equals(tipoServico.getCodigo())) {
-				lista.remove(tipoServico2);
-				break;
-			}
+
+	}
+
+	// delete
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
+
+		try {
+			business.delete(id);
+
+			return ResponseEntity.ok().build();
+		} catch (BusinessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return ResponseEntity.badRequest().body(e);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return ResponseEntity.badRequest().body(e);
 		}
-    	
-    	return ResponseEntity.ok().build();
-    	
-    }
+	}
+
+	@GetMapping("/filtro/nome")
+	public ResponseEntity<?> readByName(@PathParam("nome") String nome) {
+
+		try {
+			List<TipoServico> readByName = business.readByName(nome);
+
+			if (readByName.isEmpty())
+				return ResponseEntity.notFound().build();
+
+			return ResponseEntity.ok(readByName);
+		} catch (BusinessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return ResponseEntity.badRequest().body(e);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return ResponseEntity.badRequest().body(e);
+		}
+	}
 }
